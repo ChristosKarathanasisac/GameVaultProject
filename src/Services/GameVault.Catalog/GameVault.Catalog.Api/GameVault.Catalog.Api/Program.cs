7a562c744265e288a79c0a227f96f6b658706ad3
ChristosKarathanasisac <christoskarathanasisac@gmail.com>
2026-08-07 16:45:58 +0300

    Add Structured Logging support to the Catalog MS

--- src/Services/GameVault.Catalog/GameVault.Catalog.Api/GameVault.Catalog.Api/Program.cs
+++ src/Services/GameVault.Catalog/GameVault.Catalog.Api/GameVault.Catalog.Api/Program.cs
@@ -1,28 +1,52 @@
 using GameVault.Catalog.Infrastructure;
 using GameVault.Catalog.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-builder.Services.AddControllers();
-builder.Services.AddInfrastructure(builder.Configuration);
-builder.Services.AddOpenApi();
-
-var app = builder.Build();
-
-using (var scope = app.Services.CreateScope())
+try
 {
-    var db = scope.ServiceProvider.GetRequiredService<CatalogDbContext>();
-    await db.Database.MigrateAsync();
-}
+    Log.Information("Starting Catalog API");
 
-if (app.Environment.IsDevelopment())
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog((ctx, services, config) =>
+        config.ReadFrom.Configuration(ctx.Configuration)
+              .ReadFrom.Services(services));
+
+    builder.Services.AddControllers();
+    builder.Services.AddInfrastructure(builder.Configuration);
+    builder.Services.AddOpenApi();
+
+    var app = builder.Build();
+
+    using (var scope = app.Services.CreateScope())
+    {
+        var db = scope.ServiceProvider.GetRequiredService<CatalogDbContext>();
+        await db.Database.MigrateAsync();
+    }
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.MapOpenApi();
+    }
+
+    app.UseSerilogRequestLogging();
+
+    app.UseAuthorization();
+    app.MapControllers();
+
+    app.Run();
+}
+catch (Exception ex)
 {
-    app.MapOpenApi();
+    Log.Fatal(ex, "Catalog API terminated unexpectedly");
 }
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+finally
+{
+    // Flush any buffered log events before the process exits.
+    await Log.CloseAndFlushAsync();
+}
