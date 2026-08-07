2ae2596ae62b37cc2f7061cf914aa9006e35ef56
ChristosKarathanasisac <christoskarathanasisac@gmail.com>
2026-08-07 09:24:07 +0300

    Set up dockerfile and docker compose.

--- src/Services/GameVault.Catalog/GameVault.Catalog.Api/GameVault.Catalog.Api/Program.cs
+++ src/Services/GameVault.Catalog/GameVault.Catalog.Api/GameVault.Catalog.Api/Program.cs
@@ -1,20 +1,25 @@
+using GameVault.Catalog.Infrastructure;
+using GameVault.Catalog.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<CatalogDbContext>();
+    await db.Database.MigrateAsync();
+}
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
-
-app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
