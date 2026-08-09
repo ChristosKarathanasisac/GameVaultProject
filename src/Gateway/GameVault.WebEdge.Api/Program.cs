83db64da99ccd44dc8efddb47b528b32aa2ac99f
ChristosKarathanasisac <christoskarathanasisac@gmail.com>
2026-08-09 12:03:49 +0300

    Add the login functinality

--- src/Gateway/GameVault.WebEdge.Api/Program.cs
+++ src/Gateway/GameVault.WebEdge.Api/Program.cs
@@ -1,4 +1,5 @@
 using Serilog;
+using Yarp.ReverseProxy.Configuration;
 
 Log.Logger = new LoggerConfiguration()
     .WriteTo.Console()
@@ -14,8 +15,40 @@ try
         config.ReadFrom.Configuration(ctx.Configuration)
               .ReadFrom.Services(services));
 
+    var keycloakBaseUrl = builder.Configuration["Keycloak:BaseUrl"];
+    var keycloakRealm = builder.Configuration["Keycloak:Realm"];
+
     builder.Services.AddReverseProxy()
-        .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+        .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"))
+        .LoadFromMemory(
+            routes:
+            [
+                new RouteConfig
+                {
+                    RouteId = "keycloak-token-route",
+                    ClusterId = "keycloak-cluster",
+                    AuthorizationPolicy = "Anonymous",
+                    Match = new RouteMatch { Path = "/auth/token" },
+                    Transforms =
+                    [
+                        new Dictionary<string, string>
+                        {
+                            ["PathSet"] = $"/realms/{keycloakRealm}/protocol/openid-connect/token"
+                        }
+                    ]
+                }
+            ],
+            clusters:
+            [
+                new ClusterConfig
+                {
+                    ClusterId = "keycloak-cluster",
+                    Destinations = new Dictionary<string, DestinationConfig>
+                    {
+                        ["destination1"] = new DestinationConfig { Address = keycloakBaseUrl! }
+                    }
+                }
+            ]);
 
     var app = builder.Build();
 
